e4f7684203a8a106585fa2826d52877f58b4b997
RidoIzril <ridoizril@gmail.com>
2025-06-24 03:52:39 +0700

    update buku tamu kategori dan sesi

--- app/Http/Controllers/BukuTamuController.php
+++ app/Http/Controllers/BukuTamuController.php
@@ -4,28 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Pengunjung;
+use App\Models\Kategori;
+use App\Models\Sesi;
 use Carbon\Carbon;
 
 class BukuTamuController extends Controller
 {
     public function create()
     {
-        return view('public.buku'); 
+        $kategoris = Kategori::all();
+        $sesis = Sesi::all();
+
+        return view('public.buku', compact('kategoris', 'sesis'));
     }
 
     public function store(Request $request)
     {
         $validated = $request->validate([
-            'nama_lengkap' => 'required|string|max:255',
-            'sesi' => 'required|integer|between:1,5',
-            'jenis_kelamin' => 'required|in:L,P',
-            'tanggal_lahir' => 'required|date',
-            'alamat' => 'required|string',
-            'kategori' => 'required|in:sd,smp,sma,umum',
-            // 'no_telp' => 'nullable|string|max:20',
+            'nama_lengkap'    => 'required|string|max:255',
+            'sesi_id'         => 'required|exists:sesis,id',
+            'kategori_id'     => 'required|exists:kategoris,id',
+            'jenis_kelamin'   => 'required|in:L,P',
+            'tanggal_lahir'   => 'required|date',
+            'alamat'          => 'required|string',
+            'no_telp'         => 'nullable|string|max:20',
         ]);
 
-        // Tambahkan tanggal kunjungan (hari ini)
         $validated['tanggal'] = Carbon::today();
 
         Pengunjung::create($validated);
